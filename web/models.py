ca25886facd5207621c7d79277ec6655eafe2a63
amirhossein <ahforoughi@aut.ac.ir>
2019-08-08 14:23:42 +0430

    expence model added

--- web/models.py
+++ web/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Expence(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    
