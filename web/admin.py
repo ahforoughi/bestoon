a2910abb7f3a6968daa243e8790ef93ab0d9d940
amirhossein <ahforoughi@aut.ac.ir>
2019-08-09 01:38:48 +0430

    income in models and beutify

--- web/admin.py
+++ web/admin.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import Expence
+from .models import Income
 
 # Register your models here.
 admin.site.register(Expence)
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -9,4 +9,17 @@ class Expence(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User)
+    def __unicode__(self):
+        return "{}-{}".format(self.date, self.amount)
     
+
+      
